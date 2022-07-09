4e60e8052798fcdc41cec6a96e0e7581987d8874
Lucas Oros <ioroscampo@gmail.com>
2022-07-09 21:07:15 +0200

    clear console logs

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './src/styles/index.scss';
 import App from './src/App';
-(() => {
-    console.log('webpack worked')
-  })()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React, { useEffect } from "react";
 
  function App(props) {
 
-    useEffect(()=> {console.log("hola")});
-    const clicked = ( )=> {
-        console.log("hola");
-    }
     return (
         <div>
         <p>Hellooo</p> 
