9cefc45f1e732e2c3ebf896ad16939e1689c7ac3
lucas oros <ioroscampo@gmail.com>
2023-09-24 21:01:25 +0200

    add time entries updated

--- ViewModels/ClientOverviewVM.cs
+++ ViewModels/ClientOverviewVM.cs
@@ -12,12 +12,8 @@ namespace TempusFujit.ViewModels
     public class ClientOverviewVM : INotifyPropertyChanged
     {
         readonly IDbContextFactory<DatabaseContext> dbFactory = Services.DbFactory;
-        DateTime _newStartingDate;
-        DateTime _newEndingDate;
-        DateTime _minEndingDate;
-        public DateTime MinDate { get; } = DateTime.UtcNow.AddMonths(-1);
-        public DateTime MaxDate { get; } = DateTime.UtcNow.AddMonths(1);
 
+        #region Client
         int clientId;
         public int ClientId
         {
@@ -29,6 +25,57 @@ namespace TempusFujit.ViewModels
             }
         }
 
+        Client client;
+        public Client Client { get => client; set { client = value; OnPropertyChanged(); } }
+        private Client GetClient()
+        {
+            using var db = dbFactory.CreateDbContext();
+            return db.Clients.First(x => x.Id == clientId);
+        }
+        #endregion
+
+        #region TimeEntryInput
+        public DateTime TaskDate { get; set; }
+
+        TimeSpan taskStartingTime = DateTime.Now.TimeOfDay;
+        public TimeSpan TaskStartingTime
+        {
+            get => taskStartingTime;
+            set { taskStartingTime = value; UpdateAddButton(value, TaskEndingTime); }
+        }
+
+        TimeSpan taskEndingTime = DateTime.Now.TimeOfDay;
+        public TimeSpan TaskEndingTime
+        {
+            get => taskEndingTime;
+            set { taskEndingTime = value; UpdateAddButton(TaskStartingTime, value); }
+        }
+
+        bool isAddButtonEnabled;
+        public bool IsAddButtonEnabled { get => isAddButtonEnabled; set { isAddButtonEnabled = value; OnPropertyChanged(); } }
+
+        private void UpdateAddButton(TimeSpan initial, TimeSpan final)
+        {
+            IsAddButtonEnabled = initial < final;
+        }
+
+        void createTimeEntry()
+        {
+            using var db = dbFactory.CreateDbContext();
+            var newTimeEntry = new TimeEntry
+            {
+                ClientId = ClientId,
+                CreationDate = DateTime.UtcNow,
+                StartingTime = TaskDate.Date.Add(TaskStartingTime),
+                EndingTime = TaskDate.Date.Add(TaskEndingTime)
+            };
+            db.TimeEntries.Add(newTimeEntry);
+            db.SaveChanges();
+            Snackbar.Make("El tiempo ha sido correctamente añadido al cliente de mama", duration: TimeSpan.FromSeconds(2)).Show();
+        }
+        #endregion
+
+        #region Selected month 
         int hoursInSelectedMonth;
         public int HoursInSelectedMonth
         {
@@ -37,70 +84,21 @@ namespace TempusFujit.ViewModels
         }
         DateTime selectedMonth = DateTime.UtcNow;
         public DateTime SelectedMonth { get => selectedMonth; set { selectedMonth = value; } }
+        #endregion
 
-        private Client GetClient()
-        {
-            using var db = dbFactory.CreateDbContext();
-            return db.Clients.First(x => x.Id == clientId);
-        }
-        Client client;
-        public Client Client { get => client; set { client = value; OnPropertyChanged(); } }
-        public DateTime NewStartingDate
-        {
-            get => _newStartingDate;
-            set
-            {
-                _newStartingDate = value;
-                MinEndingDate = value;
-                this.OnPropertyChanged(PropertyChanged);
-            }
-        }
-        public DateTime NewEndingDate
-        {
-            get => _newEndingDate;
-            set
-            {
-                _newEndingDate = value;
-                this.OnPropertyChanged(PropertyChanged);
-            }
-        }
-        public DateTime MinEndingDate
-        {
-            get => _minEndingDate;
-            set
-            {
-                _minEndingDate = value;
-                this.OnPropertyChanged(PropertyChanged);
-            }
-        }
-
+        #region Property changed
         public event PropertyChangedEventHandler PropertyChanged;
-
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
         public ICommand CreateTimeEntry { get; set; }
 
         public ClientOverviewVM()
         {
+            TaskDate = DateTime.UtcNow;
             CreateTimeEntry = new Command(execute: () => createTimeEntry());
-        }
-
-        private void createTimeEntry()
-        {
-            using var db = dbFactory.CreateDbContext();
-            var newTimeEntry = new TimeEntry
-            {
-                ClientId = ClientId,
-                StartingTime = NewStartingDate,
-                EndingTime = NewEndingDate,
-                CreationDate = DateTime.UtcNow
-            };
-            db.TimeEntries.Add(newTimeEntry);
-            db.SaveChanges();
-            Snackbar.Make("El tiempo ha sido correctamente añadido al cliente de mama", duration: TimeSpan.FromSeconds(2)).Show();
-        }
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
